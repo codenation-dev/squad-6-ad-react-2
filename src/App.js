d7aa8cfafcb786566aee1ef6901bd15e5787bef4
Jorge Guimarães <jorginhoad@gmail.com>
2019-08-06 20:55:23 -0300

    refactor(app) : remove userless header tag

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from 'react'
 import { connect } from 'react-redux'
-// import { gql } from 'apollo-boost'
-// import { graphql } from 'react-apollo'
 import Main from './components/Main/Main'
 import NotFound from './components/NotFound/NotFound'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Header from './components/Header/Header'
 
-// const testQuery = gql`
-//   query {
-//     viewer {
-//       login
-//     }
-//   }
-// `
-
-function App ({ repo, errMsg }) {
+function App ({ repos, errMsg }) {
   return (
     <div className='App'>
       <Header />
@@ -25,13 +15,7 @@ function App ({ repo, errMsg }) {
           exact
           path='/'
           render={() =>
-            errMsg.length ? (
-              <NotFound msg={errMsg} />
-            ) : repo.length ? (
-              <Main />
-            ) : (
-              <h1 />
-            )
+            errMsg ? <NotFound msg={errMsg} /> : repos.length ? <Main /> : null
           }
         />
       </Router>
@@ -41,7 +25,7 @@ function App ({ repo, errMsg }) {
 
 const mapStateToProps = state => ({
   errMsg: state.user.errMsg,
-  repo: state.user.userRepos
+  repos: state.user.userRepos
 })
 
 export default connect(mapStateToProps)(App)
