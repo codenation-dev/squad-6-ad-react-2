b1c2710d0f0880bea387e78011483497f1152d63
Jorge Guimarães <jorginhoad@gmail.com>
2019-08-06 08:37:24 -0300

    refactor(rootReducer): refactor reducer to use user object

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,4 +1,4 @@
 import { combineReducers } from 'redux'
-import repositories from './ducks/repositories'
+import user from './ducks/user'
 
-export default combineReducers({ repositories })
+export default combineReducers({ user })
