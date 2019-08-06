47df4ef80c0604034d19822c50c08eda8510a63c
Jorge Guimarães <jorginhoad@gmail.com>
2019-08-06 08:15:35 -0300

    feat(user): add user to storage

---
+++ src/store/ducks/user.js
@@ -0,0 +1,42 @@
+import client from '../../services/client'
+import { userQuery } from '../../services/queries'
+
+export const TYPES = {
+  FETCH_USER: 'FETCH_USER',
+  FETCH_USER_ERROR: 'FETCH_USER_ERROR'
+}
+
+const initialState = {
+  user: {},
+  userRepos: [],
+  errMsg: ''
+}
+
+export default (state = initialState, { type, payload }) => {
+  if (type === TYPES.FETCH_USER) {
+    const user = { ...payload }
+    delete user.repositories
+    const userRepos = [...payload.repositories.nodes]
+    return { ...state, user, userRepos }
+  }
+
+  if (type === TYPES.FETCH_USER_ERROR) return { ...state, errMsg: payload }
+
+  return state
+}
+
+export const getUser = login => dispatch =>
+  client
+    .query({ query: userQuery, variables: { login } })
+    .then(({ data }) =>
+      dispatch({
+        type: TYPES.FETCH_USER,
+        payload: data.user
+      })
+    )
+    .catch(() =>
+      dispatch({
+        type: TYPES.FETCH_USER_ERROR,
+        payload: 'User not found'
+      })
+    )
