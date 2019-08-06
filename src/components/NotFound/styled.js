9594492c99006d3d2552bb950c3758dde5a0706c
Jorge Guimarães <jorginhoad@gmail.com>
2019-08-06 14:55:45 -0300

    feat(notfound):add component notfound

---
+++ src/components/NotFound/styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const DivContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 10px;
+  margin: 20px;
+`
+
+export const H1 = styled.h1`
+  font-size: 40px;
+  line-height: 20px;
+`
