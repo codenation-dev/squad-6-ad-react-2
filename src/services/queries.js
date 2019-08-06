a0dfed007807874b50ed6d360b547da9924f270a
Jorge Guimarães <jorginhoad@gmail.com>
2019-08-06 08:35:12 -0300

    feat(query):add attributes

--- src/services/queries.js
+++ src/services/queries.js
@@ -1,10 +1,16 @@
 import { gql } from 'apollo-boost'
 
-export const getRepos = gql`
+export const userQuery = gql`
   query userRepos($login: String!) {
     user(login: $login) {
+      id
+      avatarUrl
+      email
+      login
+      name
+      location
       repositories(
-        first: 10
+        first: 100
         privacy: PUBLIC
         orderBy: { field: CREATED_AT, direction: DESC }
       ) {
