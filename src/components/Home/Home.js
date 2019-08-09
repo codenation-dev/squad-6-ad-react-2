06f296b601cdabb25adb083e0ee85f39fd541c6a
Jorge Guimarães <jorginhoad@gmail.com>
2019-08-09 09:34:18 -0300

    feat(home): created component home and styled

---
+++ src/components/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { DivContainer, PTexto } from './styled'
+
+export default function Home () {
+  return (
+    <DivContainer>
+      <PTexto>Expore the public repositories from login</PTexto>
+    </DivContainer>
+  )
+}
---
+++ src/components/Home/styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const DivContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 10px;
+  margin-top: 20%;
+`
+
+export const PTexto = styled.p`
+  font-size: 40px;
+  line-height: 20px;
+`
