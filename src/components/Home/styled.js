c19a3b666a3d266cd52ecc193dffd5c6be316331
xavierleonardo <gxavier.leonardo@gmail.com>
2019-08-09 21:51:09 -0300

    fix(home): fix positioning for smaller screens

--- src/components/Home/styled.js
+++ src/components/Home/styled.js
@@ -5,9 +5,14 @@ export const DivContainer = styled.div`
   justify-content: center;
   align-items: center;
   height: calc(100vh - 69px);
+  width: 100%;
 `
 
 export const PTexto = styled.p`
   font-size: 40px;
   line-height: 20px;
+
+  @media screen and (max-width: 500px) {
+    font-size: 30px;
+  }
 `
